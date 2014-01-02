cc0ffdd482bb40fc33e0e91e37ecc0cef70f8998
Brian Sam-Bodden <bsbodden@integrallis.com>
2014-01-02 12:39:13 -0700

    Updated simplecov and coverals config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,12 @@ require 'simplecov'
 require 'coveralls'
 Coveralls.wear!
 
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
-SimpleCov.start do
-  add_filter "/spec/"
-end
+SimpleCov.command_name 'Unit Tests'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
 
 require 'rubygems'
 require 'pry'
